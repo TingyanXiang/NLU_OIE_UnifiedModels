a22dcc5b8b87d1e208afa3ccc2d1703d6670f35c
TingyanXiang <xtyshawn@gmail.com>
2019-03-29 16:38:06 -0400

    mod

--- Machine_Translation_NLP/evaluation.py
+++ Machine_Translation_NLP/evaluation.py
@@ -2,7 +2,7 @@
 import torch
 import numpy as np
 from nltk.translate import bleu_score 
-from config import SOS_token, EOS_token, PAD_token, oov_pred_index, vocab_pred
+from config import SOS_token, UNK_token, EOS_token, PAD_token, oov_pred_index, vocab_pred
 import beam
 import difflib
 
