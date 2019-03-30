697ae549104b1fd09c3608d2ca7a727871ad8fb2
TingyanXiang <xtyshawn@gmail.com>
2019-03-30 11:26:52 -0400

    mod

--- Machine_Translation_NLP/evaluation.py
+++ Machine_Translation_NLP/evaluation.py
@@ -101,7 +101,7 @@ def evaluate_batch(loader, encoder, decoder, tgt_max_length, vocab, vocab_pred_s
                         pred_match_num[pred_i] += 1
         precision[i] = pred_match_num.mean()
         recall[i] = org_match_num.mean()
-    if False:
+    if True:
         random_sample = np.random.randint(eval_len)
         print('src:', src_org[random_sample])
         print('Ref: ', tgt_org[random_sample])
